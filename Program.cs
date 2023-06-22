1085a49f4f8e3940fd485ad27d9e03d2d22a2c13
zeynep <zeynepkayhan7@gmail.com>
2023-06-22 18:21:26 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using LinqPractices.DbOperations;
+
+namespace LinqPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataGenerator.Initialize();
+            LinqDbContext _context = new LinqDbContext();
+
+            var students = _context.Students.ToList<Student>();
+        }
+    }
+}
